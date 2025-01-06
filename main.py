6c729b3419c04cf698a121e48838522c6a88b550
manu-xcaliber <manu.gupta@xcaliber.health>
2025-01-06 06:36:32 +0000

    working blank window

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ from constants import *
 import pygame 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     print("Starting asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
