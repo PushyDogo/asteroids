b1462731b10622317f1404043959b177a563c225
manu-xcaliber <manu.gupta@xcaliber.health>
2025-01-06 13:36:38 +0000

    collision detection with the player

--- main.py
+++ main.py
@@ -34,6 +34,11 @@ def main():
         for obj in updatable:
             obj.update(dt)
         
+        for obj in asteroids:
+            if obj.collision(player):
+                print("Game over!")
+                return
+
         screen.fill("black")
 
         for obj in drawable:
