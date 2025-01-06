c8d849f1c91883e3155b873ae3b4a4a575148114
manu-xcaliber <manu.gupta@xcaliber.health>
2025-01-06 14:07:31 +0000

    shots kill asteroids

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main():
     Asteroid.containers = (updatable, drawable, asteroids)
     AsteroidField.containers = (updatable)
     Player.containers = (updatable, drawable)
-    Shot.containers = (updatable, drawable)
+    Shot.containers = (updatable, drawable, shots)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
@@ -41,6 +41,11 @@ def main():
             if obj.collision(player):
                 print("Game over!")
                 return
+            
+            for shot in shots:
+                if obj.collision(shot):
+                    obj.kill()
+                    shot.kill()
 
         screen.fill("black")
 
